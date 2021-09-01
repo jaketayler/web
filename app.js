c3406477f98f3440df893d249a4659219e940186
Jake Tayler <thainav2010@gmail.com>
2021-09-01 13:40:23 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,63 @@
+var products = [];
+
+var product = null; 
+
+
+function NewProduct(){
+  return{
+    Codigo: "",
+    Descricao: ""
+  };
+}
+
+function adicionar(){
+  product = NewProduct();
+
+  $("#frmList").hide();
+  $("#frmCad").show();
+
+  $("#cod").val("");
+  $("#desc").val("");
+}
+
+function cancelar(){
+  $("#frmList").show();
+  $("#frmCad").hide();
+}
+
+function save(){
+
+  product.Codigo = $("#cod").val();
+  product.Descricao = $("#desc").val();
+
+  products.push(product);
+  loadingProducts();
+ 
+  $("#cod").val("");
+  $("#desc").val("");
+  
+  $("#frmList").show();
+  $("#frmCad").hide();
+
+}
+
+function loadingProducts(){
+  products.map(function(prod){
+    $("#tblProducts tbody").append(
+      "<tr>"+
+        "<td>" + prod.Codigo + "</td>"+
+        "<td>" + prod.Descricao + "</td>"+
+        "<td> <button class='btn btn-primary'>Editar</button> <button class='btn btn-danger'>Remover</button> </td>"+
+      "</tr>"
+    )
+  });
+}
+
+$(document).ready(function(){
+  $("#frmCad").hide();
+
+  $(document).on("click", "#btnAdd", adicionar);
+  $(document).on("click", "#btnCanelar", cancelar);
+  $(document).on("click", "#btnSave", save);
+
+});

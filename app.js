0639fdd485d4697756af43ada63c23a4d1842a18
Jake Tayler <thainav2010@gmail.com>
2021-09-09 22:27:42 -0300

    Aula dia 09-09/2021

--- app.js
+++ app.js
@@ -38,7 +38,23 @@ function save(){
 
   $("#frmList").show();
   $("#frmCad").hide();
+}
 
+function edit(){
+  $(".btn-edit").each(function(){
+      var codProduct = $(this).data("codigo");
+      var prodSearch = products.filter(function(prod){
+        return prod.Codigo == codProduct;
+      })[0];
+      
+      console.log(prodSearch);
+
+      $("#cod").val(product.Codigo);
+      $("#desc").val(product.Descricao);
+
+      $("#frmList").hide();
+      $("#frmCad").show();
+    });
 }
 
 function loadingProducts(){
@@ -47,10 +63,12 @@ function loadingProducts(){
       "<tr>"+
         "<td>" + prod.Codigo + "</td>"+
         "<td>" + prod.Descricao + "</td>"+
-        "<td> <button class='btn btn-primary'><i class='fas fa-edit'></i>Editar</button> <button class='btn btn-danger'> <i class='fas fa-trash-alt'></i>Remover</button> </td>"+
+        "<td> <button class='btn btn-edit btn-primary mr-5' data-codigo='" + prod.Codigo + "' onclick='edit()'><i class='fas fa-edit'></i>Editar</button> "+
+         "<button class='btn btm-remove btn-danger ml-5' data-codigo='"+ prod.Codigo + "'> <i class='fas fa-trash-alt'></i>Remover</button> </td>"+
       "</tr>"
     )
   });
+  edit();
 }
 
 $(document).ready(function(){
